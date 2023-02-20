2851dac073d2a9d271bd5c4ddfd2e566eddf5a4c
unknown <nicolasmail209@gmail.com>
2023-02-19 22:45:38 -0300

    Adds DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      //host: "localhost",
+      //port: 3306,
+      //username: "test",
+      //password: "test",
+      database: "database",
+      synchronize: true,
+      //logging: false,
+      entities: [
+        //"src/entity/**/*.ts"
+        __dirname + '/**/*.entity{.ts,.js}'
+      ]
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
